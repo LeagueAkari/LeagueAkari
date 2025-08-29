a8169431337320f18dd3876d933a99636827c626
Hanxven <hanxven@gmail.com>
2025-08-29 16:50:17 +0800

    dep: update

--- src/renderer-shared/compositions/useSgpTagOptions.ts
+++ src/renderer-shared/compositions/useSgpTagOptions.ts
@@ -1,5 +1,6 @@
 import { useLeagueClientStore } from '@renderer-shared/shards/league-client/store'
 import { useTranslation } from 'i18next-vue'
+import { SelectMixedOption } from 'naive-ui/es/select/src/interface'
 import { computed } from 'vue'
 
 const QUEUE_IDS = [420, 430, 440, 450, 480, 1700, 490, 1900, 900, 2300, 4210, 4220, 4250]
@@ -18,6 +19,6 @@ export function useSgpTagOptions() {
         label: lcs.gameData.queues[id]?.name || id,
         value: `q_${id}`
       }))
-    ]
+    ] as SelectMixedOption[]
   })
 }
